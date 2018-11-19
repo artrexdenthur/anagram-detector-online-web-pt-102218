610b7450715e699ffb3ca688052f2633923a57e6
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 03:32:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(ana_arr)
+    # return all anagram matches in an array
+    matches = []
+    ana_arr.each do |ana|
+      if ana.sort
+  
+end
